612f0aa8e0e73504f38579692f8b7d6097cb9ea9
Sorongoku <gonzalorpg@gmail.com>
2018-06-09 17:24:48 -0300

    main inicial

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <functional>
+#include <tuple>
+#include <stdlib.h>
+
+
+#include <cmath>
+#include <chrono>
+
+using namespace std;
+
+int main(int argc, char * argv[]) {
+
+    return 0;
+}
+
