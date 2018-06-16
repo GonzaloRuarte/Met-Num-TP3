e1acc937424987d13ae29b094c8acdca24fff4b6
christian <clbonomi@gmail.com>
2018-06-16 20:53:48 -0300

    funcion que mide el tiempo que tarda la señal en atravesar el cuerpo

--- main.cpp
+++ main.cpp
@@ -13,8 +13,27 @@
 
 using namespace std;
 
-int main(int argc, char * argv[]) {
-
-    return 0;
+/**
+ * esta funcion toma como parametros las matrices D y V
+ * @return el tiempo que tarda la senial en atravesar el cuerpo
+ */
+double medirTiempoDeRetrasoDeSenial(vector<vector<double>>& D, vector<vector<double>>& V) {
+    double ret = 0.0;
+    int n = D.size();
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<n; j++) {
+            ret += D[i][j] * V[i][j];
+        }
+    }
 }
 
+int main(int argc, char * argv[]) {
+
+    vector<vector<double>> D = {{1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {0,0,0,1}};
+    vector<vector<double>> V = {{0,1,2,3}, {4,5,6,7}, {8,9,10,11}, {14,25,36,57}};
+
+    double t = medirTiempoDeRetrasoDeSenial(D, V);
+
+    cout << t;
+    return 0;
+}
