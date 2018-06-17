b8a2f386cdd5b038093d893110f35b88659601ca
Rindoresu <borgnia9@hotmail.com>
2018-06-17 19:33:05 -0300

    Agregue un CMakeLists.txt e hice en c++ la funcion que dibuja la matriz D de matlab. Arregle el problema que tenia mi implementación, y cambie la interpretación de los puntos que era un poco confusa.

--- main.cpp
+++ main.cpp
@@ -14,6 +14,65 @@
 using namespace std;
 
 /**
+ * Crea una matriz D_k del mismo tamaño que la imágen que será el resultado (nxn), donde un 1 en esa posición indica
+ * que la recta pasa por ese pixel. (osea, un 1 en D[0][37] indica que el rayo pasa por el pixel de la fila 1,
+ * de la columna 38).
+ * @param p1 es el primer punto, la función funciona para cualquier punto inicial EN EL BORDE
+ * (no me fije que pasa si no esta en el borde, quizá ande)
+ * A diferencia de matlab, decidi que para mejorar la comprensión, queda mas claro si lo pensamos matricial y no por ejes.
+ * O sea, p1= [3,7], significa que p1 es el punto en el pixel de la fila 4, la columna 8. En matlab hice que sea
+ * el punto 3 en el eje x, 7 en el eje y.
+ * Por último el algoritmo supone que p1 y p2 son distintos, si son el mismo no funciona.
+ * @param p2 es el segundo punto, donde termina la recta (lo mismo que el primer punto, puede ser cualquiera pero en borde)
+ * @param n es el tamaño de la matriz resultado del método que estamos desarrollando (Por ahora sería cuadrada).
+ * @param result es la matriz resultante con unos donde pasa el rayo, en este caso del tp sería la matriz D^k.
+ */
+vector<vector<double> > trazar_recta_en_matriz_D(pair<int,int> p1, pair<int,int> p2, int n) {
+    vector<vector<double> > result(n, vector<double>(n,0));
+    int inicio;
+    int fin;
+    if(p1.second == p2.second) {
+        // La recta es vertical, la trazo y termino el método.
+        for(int i = 0; i<n ; i++) {
+            result[i][p1.second] = 1;
+        }
+        return result;
+    } else if(p1.second < p2.second) { //me fijo cual va primero.
+        inicio = p1.second;
+        fin = p2.second;
+    } else {
+        fin = p1.second;
+        inicio = p2.second;
+    }
+    double a = (double(p2.first) - double(p1.first)) / (double(p2.second) - double(p1.second)); // No importa el orden
+            // de p1 y p2, calcula la pendiente, fila 2 menos fila 1 sobre columna 2 menos columna 1.
+    double b = double(p1.first) - a * double(p1.second); // calcula b = y - ax, con el punto p1.
+    for(int i = inicio; i<fin ; i++) { //voy de la columna inicio a la fin, pintando los pixeles por los que pase.
+        int pintar_desde = floor(a*i + b);
+        int pintar_hasta = floor(a*(i+1) +b);
+        if (pintar_hasta < pintar_desde) {
+            int aux = pintar_desde; //si estan al revez los swappeo
+            pintar_desde = pintar_hasta;
+            pintar_hasta = aux;
+        }
+        for(int j = pintar_desde; j <= pintar_hasta ; j++) {
+            result[j][i] = 1;
+        }
+    }
+    int pintar_desde = floor(a*fin + b);
+    int pintar_hasta = floor(a*(fin+1) +b);
+    if (pintar_hasta < pintar_desde) {
+        int aux = pintar_desde; //si estan al revez los swappeo
+        pintar_desde = pintar_hasta;
+        pintar_hasta = aux;
+    }
+    for(int j = pintar_desde; j <= pintar_hasta ; j++) {
+        result[j][fin] = 1;
+    }
+    return result;
+}
+
+/**
  * esta funcion toma como parametros las matrices D y V
  * @return el tiempo que tarda la senial en atravesar el cuerpo
  */
