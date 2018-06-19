b4a7086c17c0a2e42d07661c29d585119ba0b328
christian <clbonomi@gmail.com>
2018-06-19 06:49:51 -0300

    agrego la lectura de imagenes

---
+++ util.cpp
@@ -0,0 +1,5 @@
+//
+// Created by christian on 19/06/18.
+//
+
+#include "util.h"
---
+++ util.h
@@ -0,0 +1,14 @@
+//
+// Created by christian on 19/06/18.
+//
+
+#ifndef PAGERANK_UTIL_H
+#define PAGERANK_UTIL_H
+
+
+class util {
+
+};
+
+
+#endif //PAGERANK_UTIL_H
