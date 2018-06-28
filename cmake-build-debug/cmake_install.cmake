5a6461c7b47a52f0095c468876e05b1f9ce1fef8
Sir Tristepin <nricca@dc.uba.ar>
2018-06-27 21:15:12 -0300

    Hice la función "experimentacion".

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/nahuel/Escritorio/UBA/Mét. num./TP3/Código
+# Install script for directory: /home/nahuel/Escritorio/UBA/Mét. num./TP3/Codigo
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/nahuel/Escritorio/UBA/Mét. num./TP3/Código/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/nahuel/Escritorio/UBA/Mét. num./TP3/Codigo/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- test_main.cpp
+++ test_main.cpp
@@ -44,30 +44,39 @@ int main() {
 
     D = trazar_recta_en_matriz_D(Laseres[0],Miras[0],500);
     */
-    vector<unsigned char> discretizaciones = {8, 16, 32};
+    vector<unsigned short int> discretizaciones = {32, 64};
     vector<pair<float,float> > ruidos = {make_pair(0.01, 0.04), make_pair(0.05, 0.09)};
     vector<unsigned short int> espacios_entre_censores = {4, 8, 16};
     string directorio = "Imagenes_para_probar";
-    //experimentacion_barrido_H(directorio, 512, discretizaciones, ruidos, espacios_entre_censores);
     vector<string> archivos;
+    listarDirectorio(directorio, archivos);
+    uint tamanio_imagenes = 512;
+    vector<unsigned short int> cantidades_de_fuentes = {4, 8, 16, 32, 64};
+    vector<unsigned short int> separaciones = {1, 2, 4, 6, 8, 12, 16};
+    //experimentacion_barrido_H(directorio, tamanio_imagenes, discretizaciones, ruidos, espacios_entre_censores);
+/*    vector<string> archivos;
     listarDirectorio(directorio, archivos);
     for(size_t ind_disc = 0; ind_disc < discretizaciones.size(); ++ind_disc) {
         for(size_t ind_espac = 0; ind_espac < espacios_entre_censores.size(); ++ind_espac) {
+            ofstream salida("resultados de prueba/Discretización:" + to_string(discretizaciones[ind_disc]) + " espaciado:" +to_string(espacios_entre_censores[ind_espac]) + " .txt");
+            salida.close();
             for(size_t ind_arch = 0; ind_arch < archivos.size(); ++ind_arch) {
-                ofstream salida("resultados de prueba/Discretización:" + to_string(discretizaciones[ind_disc]) + " espaciado:" +to_string(espacios_entre_censores[ind_espac]) + " .txt");
+                salida.open("resultados de prueba/Discretización:" + to_string(discretizaciones[ind_disc]) + " espaciado:" +to_string(espacios_entre_censores[ind_espac]) + " .txt", ios::app);
                 salida << "Imagen " + archivos[ind_arch] + ":\t";
                 salida.close();
                 for(size_t ind_ruido = 0; ind_ruido < ruidos.size(); ++ind_ruido){
                     salida.open("resultados de prueba/Discretización:" + to_string(discretizaciones[ind_disc]) + " espaciado:" +to_string(espacios_entre_censores[ind_espac]) + " .txt", ios::app);
-                    salida << discretizaciones[ind_disc] << ", " << espacios_entre_censores[ind_espac] << ", " << ruidos[ind_ruido].first << ", " << ",\t";
+                    salida << discretizaciones[ind_disc] << ", " << espacios_entre_censores[ind_espac] << ", " << ruidos[ind_ruido].first << ",\t";
                     salida.close();
                 }
                 salida.open("resultados de prueba/Discretización:"+to_string(discretizaciones[ind_disc])+" espaciado:"+to_string(espacios_entre_censores[ind_espac])+" .txt", ios::app);
-                salida << endl;
-                salida << endl; //Lo hago 2 veces para mejor visibilidad.
+                salida << endl << endl; //Lo hago 2 veces para mejor visibilidad.
                 salida.close();
             }
         }
-    }
+    }*/
+    experimentacion('h', archivos, tamanio_imagenes, discretizaciones, cantidades_de_fuentes, separaciones, ruidos);
+    experimentacion('v', archivos, tamanio_imagenes, discretizaciones, cantidades_de_fuentes, separaciones, ruidos);
+    experimentacion('r', archivos, tamanio_imagenes, discretizaciones, cantidades_de_fuentes, separaciones, ruidos);
     return 0;
 }
